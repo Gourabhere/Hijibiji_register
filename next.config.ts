74462a381d8ae384a30441103bf0edcaac2c93d5
GOURAB SAHA <gourabhere@gmail.com>
2025-07-12 03:43:48 +0000

    use this to access and modify the sheet
    {
      "type": "service_account",

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    GOOGLE_SCRIPT_API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwTnL1RGjCHKsMsrRyw0bNRBfReAKeut0uu6gdszOLXZ6SAJbG2LYl-UYPW-41caxmPuA/exec',
+    GOOGLE_SCRIPT_API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbyE4TVE1ArcT4QFd2ij7v-8t_fBg_fXR3JmJCHkbcFEs2VIVTk8VHAARwNu8iVywfcGKg/exec',
     GOOGLE_SHEETS_ID: '1z_6CN-5qHWvnphs8H3fNSldQVQHc9X6qxutyW66MwUw',
     GOOGLE_SHEET_NAME: 'Sheet1',
   },
